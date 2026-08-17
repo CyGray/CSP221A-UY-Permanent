602065f0f1efecd2a357570a8317f940debb088a
cygray <work.uykyleyuan@gmail.com>
2026-08-17 18:45:21 +0800

    feat(runner): add safe task execution

--- robot.py
+++ robot.py
@@ -82,3 +82,14 @@ class CoatedNutRobot(Robot):
 def fleet_report(robots):
     for robot in robots:
         print(str(robot))
+
+
+def run_task_safely(robot, **kwargs):
+    try:
+        result = robot.perform_task(**kwargs)
+    except InsufficientBatteryError as error:
+        logging.error("%s", error)
+    else:
+        print(result)
+    finally:
+        print(f"{robot.name} has {robot.battery}% battery.")
