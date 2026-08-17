4dc709507262048cf49bc96cc111123461f5aa6e
cygray <work.uykyleyuan@gmail.com>
2026-08-17 18:45:57 +0800

    feat(core): add from_config classmethod

--- robot.py
+++ robot.py
@@ -34,6 +34,10 @@ class Robot(abc.ABC):
         self.battery = battery
         Robot.population += 1
 
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
     @property
     def battery(self):
         return self._battery
